c846a0131bdffdd2404db4933ec50542f862616c
mrkaye97 <mrkaye97@gmail.com>
2025-01-31 15:48:40 -0500

    feat: initial pass at hatchet function

--- examples/simple/worker.py
+++ examples/simple/worker.py
@@ -3,20 +3,17 @@ from hatchet_sdk import BaseWorkflow, Context, Hatchet
 hatchet = Hatchet(debug=True)
 
 
-class MyWorkflow(BaseWorkflow):
-    @hatchet.step(timeout="11s", retries=3)
-    def step1(self, context: Context) -> dict[str, str]:
-        print("executed step1")
-        return {
-            "step1": "step1",
-        }
+@hatchet.function(timeout="11s")
+def step1(context: Context) -> dict[str, str]:
+    print("executed step1")
+    return {
+        "step1": "step1",
+    }
 
 
 def main() -> None:
-    wf = MyWorkflow()
-
     worker = hatchet.worker("test-worker", max_runs=1)
-    worker.register_workflow(wf)
+    worker.register_function(step1)
     worker.start()
 
 
